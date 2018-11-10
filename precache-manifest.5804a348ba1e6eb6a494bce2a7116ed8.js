085a8d5cca8407439c865d220b944b9ca353bc95
Shubham Kumar <shubham2270@gmail.com>
2018-11-10 13:19:55 +0530

    react app

---
+++ precache-manifest.5804a348ba1e6eb6a494bce2a7116ed8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d67c4efe1e4477f1c018",
+    "url": "/static/js/main.d67c4efe.chunk.js"
+  },
+  {
+    "revision": "25e4eb9fff5a6e1a4006",
+    "url": "/static/js/1.25e4eb9f.chunk.js"
+  },
+  {
+    "revision": "d67c4efe1e4477f1c018",
+    "url": "/static/css/main.bab3dff4.chunk.css"
+  },
+  {
+    "revision": "25e4eb9fff5a6e1a4006",
+    "url": "/static/css/1.8b3f71e6.chunk.css"
+  },
+  {
+    "revision": "545019b4c1725ddfb80466a901ef4811",
+    "url": "/index.html"
+  }
+];
